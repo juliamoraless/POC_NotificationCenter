d8ea0e9636da112821d3acdfcaa7df7722bb453d
Julia Morales <juliamoraless1204@gmail.com>
2024-06-10 14:22:33 -0300

    add image

--- POC_NotificationCenter/NotificationExtension/NotificationService.swift
+++ POC_NotificationCenter/NotificationExtension/NotificationService.swift
@@ -4,9 +4,7 @@
 //
 //  Created by Julia Morales on 04/06/24.
 //
-
 import UserNotifications
-import SwiftUI
 
 class NotificationService: UNNotificationServiceExtension {
 
@@ -16,21 +14,43 @@ class NotificationService: UNNotificationServiceExtension {
     override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
         self.contentHandler = contentHandler
         bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
-        
+
         if let bestAttemptContent = bestAttemptContent {
-            // Modify the notification content here...
-            bestAttemptContent.title = "\(bestAttemptContent.title) [modified]"
-            
+            if let imageURLString = bestAttemptContent.userInfo["imageURL"] as? String, let imageURL = URL(string: imageURLString) {
+                downloadAndAttachImage(from: imageURL, to: bestAttemptContent)
+            }
+        }
+    }
+
+    override func serviceExtensionTimeWillExpire() {
+        if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
             contentHandler(bestAttemptContent)
         }
     }
     
-    override func serviceExtensionTimeWillExpire() {
-        // Called just before the extension will be terminated by the system.
-        // Use this as an opportunity to deliver your "best attempt" at modified content, otherwise the original push payload will be used.
-        if let contentHandler = contentHandler, let bestAttemptContent =  bestAttemptContent {
-            contentHandler(bestAttemptContent)
-        }
-    }
+    private func downloadAndAttachImage(from url: URL, to content: UNMutableNotificationContent) {
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let data = data {
+                // Criar um arquivo temporário para a imagem
+                let fileManager = FileManager.default
+                let temporaryFolderName = ProcessInfo.processInfo.globallyUniqueString
+                let temporaryFolderURL = fileManager.temporaryDirectory.appendingPathComponent(temporaryFolderName)
 
+                do {
+                    try fileManager.createDirectory(at: temporaryFolderURL, withIntermediateDirectories: true, attributes: nil)
+                    let imageFileURL = temporaryFolderURL.appendingPathComponent("image.jpg")
+                    try data.write(to: imageFileURL)
+
+                    // Criar o attachment
+                    let attachment = try UNNotificationAttachment(identifier: "image", url: imageFileURL, options: nil)
+                    content.attachments = [attachment]
+                    self.contentHandler?(content)
+                } catch {
+                    print("Erro ao salvar a imagem: (error)")
+                }
+            }
+        }
+        task.resume()
+    }
 }
+
